e39c288a6330634a1ae294321184c1b5a2c0b3e1
Nelson Alejandro <u202310950@upc.edu.pe>
2025-10-27 20:44:24 -0500

    soporte--tecnico

--- src/app/soporte-tecnico/soporte-tecnico.ts
+++ src/app/soporte-tecnico/soporte-tecnico.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-soporte-tecnico',
   imports: [],
   templateUrl: './soporte-tecnico.html',
   styleUrl: './soporte-tecnico.css',
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SoporteTecnico {
 
