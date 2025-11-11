97edc459b58235166a20021b275989bea4e26bec
Nelson Alejandro <u202310950@upc.edu.pe>
2025-11-11 01:23:23 -0500

    carry4

--- src/app/asesor/asesorsoporte/asesorsoporte.ts
+++ src/app/asesor/asesorsoporte/asesorsoporte.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 @Component({
   selector: 'app-asesorsoporte',
   imports: [],
   templateUrl: './asesorsoporte.html',
   styleUrl: './asesorsoporte.css',
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Asesorsoporte {
 
--- src/app/usuario/usuarionosotros/usuarionosotros.ts
+++ src/app/usuario/usuarionosotros/usuarionosotros.ts
@@ -3,9 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-usuarionosotros',
   imports: [],
+  standalone: true,
   templateUrl: './usuarionosotros.html',
   styleUrl: './usuarionosotros.css',
 })
 export class Usuarionosotros {
 
+  equipo = [
+    {
+      nombre: 'Lehonel',
+      rol: 'Ingenieria de Sistemas',
+      imagen: 'assets/img/lehonel.webp',
+    },
+    {
+      nombre: 'Nelson Alejandro Espinoza',
+      rol: 'Ingenieria de Sistemas',
+      imagen: 'assets/img/nelson.webp',
+    },
+    {
+      nombre: 'Javier Hugo Chumpitazi',
+      rol: 'Ingenieria de Sistemas',
+      imagen: 'assets/img/javier.webp',
+    },
+    {
+      nombre: 'José Matos Apoloni',
+      rol: 'Ingenieria de Sistemas',
+      imagen: 'assets/img/jose.webp',
+    },
+    {
+      nombre: 'Jhair Israel Martel Paez',
+      rol: 'Ingenieria de Sistemas',
+      imagen: 'assets/img/jhair.webp',
+    },
+  ];
 }
